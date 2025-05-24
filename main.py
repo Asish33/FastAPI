63bc5fedbfa6d095470c99ba0ee5a53e45403f44
Balaji Asish <balajiashish02@gmail.com>
2025-05-24 18:17:20 +0530

    add put route

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ todos : list[Todo]=[]
 
 app = FastAPI()
 
-@app.get('/todo', response_model=list[Todo])
+@app.get('/todos', response_model=list[Todo])
 def get_all_todos():
     return todos
 
@@ -15,9 +15,18 @@ def add_todo(todo:Todo):
     todos.append(todo)
     return todo
 
-@app.get('/Todo/{todo_id}', response_model=Todo)
+@app.get('/todos/{todo_id}', response_model=Todo)
 def todo_by_id(todo_id:int):
     for todo in todos:
         if todo.id == todo_id:
             return todo
     raise HTTPException(status_code=404, detail='No Todo Present with the given id')
+
+@app.put('/todos/{todo_id}', response_model=Todo)
+def update_todo_by_id(todo_id: int, updated_todo: Todo):
+    for index, todo in enumerate(todos):
+        if todo.id == todo_id:
+            todos[index] = updated_todo  
+            return updated_todo
+    raise HTTPException(status_code=404, detail='No Todo present with given Id')
+    
